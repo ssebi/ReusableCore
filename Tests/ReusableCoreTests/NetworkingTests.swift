e36623f05f88bbdcee1cb9badc3a3e270414d0d7
Sebastian Vidrea <vidreasebastian@gmail.com>
2020-10-22 12:22:34 +0300

    Added first Network API

--- Tests/ReusableCoreTests/NetworkingTests.swift
+++ Tests/ReusableCoreTests/NetworkingTests.swift
@@ -6,14 +6,27 @@
 //
 
 import XCTest
+@testable import ReusableCore
 
 final class NetworkingTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoadDataCall() {
+        let manager = ReusableCore.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard let url = URL(string: "https://raywenderlich.com") else {
+            return XCTFail("Could not create URL properly")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testLoadDataCall", testLoadDataCall)
     ]
 }
